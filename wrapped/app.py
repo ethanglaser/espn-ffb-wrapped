8f9eef35e8f8f253ff43f9df6e8e86880e77f353
ethanglaser <glasere@purdue.edu>
2022-01-22 19:51:13 -0500

    testing if generated files deleted

--- wrapped/app.py
+++ wrapped/app.py
@@ -14,9 +14,13 @@ def data():
             league_id = request.form.get("league_id", False)
             season_id = request.form.get("season_id", False)
             swid, espn_s2 = request.form.get("swid", False), request.form.get("espn_s2", False)
+            aa = os.listdir(os.path.join(current_dir, 'templates/'))
             for file in os.listdir(os.path.join(current_dir, 'templates/')):
                 if 'generated' in str(file):
                     os.remove(os.path.join(current_dir, 'templates/' + file))
+            bb = os.listdir(os.path.join(current_dir, 'templates/'))
+            if str(league_id) == '1303917':
+                return f"{aa} {bb}"
             a = get_h2h(league_id, season_id, swid, espn_s2)
             b = get_draft_df(league_id, season_id, swid, espn_s2)
             # try:
